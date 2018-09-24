2d1259225ad30a0e1e0d0250a611728cee234604
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2018-09-24 15:48:43 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE data;
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+
+CREATE SCHEMA best;
+
+CREATE ROLE best_writer WITH PASSWORD 'best_writer';
+GRANT SELECT, UPDATE, INSERT TO ALL TABLES IN SCHEMA best_writer;
+
+CREATE ROLE best_reader WITH PASSWORD 'best_reader';
+GRANT SELECT ON ALL TABLES IN SCHEMA best TO best_reader;
+
+CREATE UNLOGGED TABLE best.cities (
+    id VARCHAR(16) PRIMARY KEY,
+    region CHAR(1) NOT NULL,
+    name_nl VARCHAR(60),
+    name_fr VARCHAR(60)
+);
+
+CREATE UNLOGGED TABLE best.streets (
+    id VARCHAR(16) PRIMARY KEY,
+    region CHAR(1) NOT NULL,
+    name_nl VARCHAR(60),
+    name_fr VARCHAR(60),
+    city_id VARCHAR(16) REFERENCES best.cities(id)
+);
+
+CREATE UNLOGGED TABLE best.addresses (
+    id VARCHAR(16) PRIMARY KEY,
+    region CHAR(1) NOT NULL,
+    streetno VARCHAR(16) NOT NULL,
+    boxno VARCHAR(24),
+    street_id VARCHAR(16) REFERENCES best.streets(id),
+    city_id VARCHAR(16) REFERENCES best.cities(id),
+    x DOUBLE PRECISION NOT NULL,
+    y DOUBLE PRECISION NOT NULL,
+    geom GEOMETRY(Point, 4326) NOT NULL
+);
