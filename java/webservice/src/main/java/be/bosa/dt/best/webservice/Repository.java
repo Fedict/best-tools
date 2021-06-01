e315eacb2b69bd27fa4525b44d97b0e46ae5f320
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2021-06-01 16:16:17 +0100

    Directly use datasource instead of ORM Signed-off-by:Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- java/webservice/src/main/java/be/bosa/dt/best/webservice/Repository.java
+++ java/webservice/src/main/java/be/bosa/dt/best/webservice/Repository.java
@@ -7,6 +7,7 @@ package be.bosa.dt.best.webservice;
 
 import be.bosa.dt.best.webservice.entities.AddressDistance;
 import io.agroal.api.AgroalDataSource;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -43,17 +44,26 @@ public class Repository {
 		List<AddressDistance> list = new ArrayList<>();
 		String params[] = new String[] { String.valueOf(x), String.valueOf(y), 
 										String.valueOf(x), String.valueOf(y), String.valueOf(maxdist) };
-		try(	PreparedStatement stmt = ds.getConnection().prepareStatement(SQL_DISTANCE, params);
-				ResultSet res = stmt.executeQuery()) {
-			while (res.next()) {
-				list.add(
-					new AddressDistance(
+		
+		try(Connection conn = ds.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(SQL_DISTANCE)) {
+				stmt.setDouble(1, x);
+				stmt.setDouble(2, y);
+				stmt.setDouble(3, x);
+				stmt.setDouble(4, y);
+				stmt.setDouble(5, maxdist);
+		
+				try(ResultSet res = stmt.executeQuery()) {
+					while (res.next()) {
+						list.add(
+							new AddressDistance(
 						res.getString(1), res.getString(2), res.getString(3), res.getString(4), 
 						res.getDouble(5), res.getDouble(5), res.getObject(6), res.getString(7),
 						res.getString(8), res.getString(9), res.getString(10), res.getString(11),
 						res.getString(12), res.getString(13), res.getString(14), res.getString(15), res.getString(16),
 						res.getString(17), res.getString(18), res.getString(19), res.getString(20), res.getString(21),
 						res.getDouble(22)));
+				}
 			}
 		} catch (SQLException ex) {
 			throw new WebApplicationException(ex);
